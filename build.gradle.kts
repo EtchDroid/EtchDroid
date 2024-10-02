997dc0b1d79722c0f3a45edb82da543348208d52
Davide Depau <davide@depau.eu>
2024-10-02 22:33:56 +0200

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     val kotlinVersion by extra("1.9.20")  // Also update the Gradle plugin version below!
 }
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
