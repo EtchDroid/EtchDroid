17a9ebc69b098d3526b3c1d8d940094f7a7e6e78
Davide Depau <davide@depau.eu>
2024-10-02 22:53:23 +0200

    Initial dependency upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["composeVersion"] as String
@@ -83,52 +84,52 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${rootProject.extra["kotlinVersion"]}")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.lifecycle:lifecycle-service:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    val composeVersion = rootProject.extra["composeVersion"] as String
+    val composeBomVersion = rootProject.extra["composeBomVersion"] as String
+    val kotlinVersion = rootProject.extra["kotlinVersion"] as String
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-service:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
 
     // Compose
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
-    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["composeVersion"]}")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.compose.ui:ui:${composeVersion}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${composeVersion}")
+    implementation("androidx.compose.material3:material3:1.3.0")
     implementation("com.google.accompanist:accompanist-navigation-animation:0.32.0")
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.7.3")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("com.airbnb.android:lottie-compose:6.2.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("io.coil-kt:coil-gif:2.5.0")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
 
     // Core dependencies
     implementation("me.jahnen.libaums:core:0.10.0")
     implementation("me.jahnen.libaums:libusbcommunication:0.3.0")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
 
     // Google Play
     "gplayImplementation"("com.google.android.play:review:2.0.1")
     "gplayImplementation"("com.google.android.play:review-ktx:2.0.1")
 
     // Test dependencies
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testImplementation("androidx.test:core:1.5.0")
+    testImplementation("androidx.test:core:1.6.1")
     testImplementation("org.mockito:mockito-core:5.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.7.3")
-    androidTestImplementation(
-        "androidx.compose.ui:ui-test-junit4:1.5.4"
-    )
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
-    debugImplementation(
-        "androidx.compose.ui:ui-test-manifest:${rootProject.extra["composeVersion"]}"
-    )
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
+    androidTestImplementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
+    debugImplementation("androidx.compose.ui:ui-tooling:${composeVersion}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${composeVersion}")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 buildscript {
-    val composeVersion by extra("1.5.4")
-    val kotlinVersion by extra("1.9.20")  // Also update the Gradle plugin version below!
+    val composeVersion by extra("1.5.15")
+    val composeBomVersion by extra("2024.09.03")
+    val kotlinVersion by extra("1.9.25")  // Also update the Gradle plugin version below!
 }
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
