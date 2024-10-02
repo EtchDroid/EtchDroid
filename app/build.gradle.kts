e0c03662f9b0efb0828f4dac707cfad7b7374ea1
Davide Depau <davide@depau.eu>
2024-10-02 23:30:26 +0200

    Migrate to Gradle catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
-    id("de.mannodermaus.android-junit5") version "1.10.0.0"
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.android.junit5)
 }
 
 android {
@@ -73,7 +73,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["composeVersion"] as String
+        kotlinCompilerExtensionVersion = libs.versions.compose.asProvider().get()
     }
     testOptions {
         unitTests.all {
@@ -84,52 +84,41 @@ android {
 }
 
 dependencies {
-    val composeVersion = rootProject.extra["composeVersion"] as String
-    val composeBomVersion = rootProject.extra["composeBomVersion"] as String
-    val kotlinVersion = rootProject.extra["kotlinVersion"] as String
+    implementation(libs.accompanist.navigation.animation)
+    implementation(libs.accompanist.permissions)
+    implementation(libs.activity.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.constraintlayout.compose)
+    implementation(libs.core.ktx)
+    implementation(libs.kotlin.stdlib.jdk8)
+    implementation(libs.kotlinx.coroutines.debug)
+    implementation(libs.libaums.core)
+    implementation(libs.libaums.libusbcommunication)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.service)
+    implementation(libs.lottie.compose)
+    implementation(libs.material)
+    implementation(libs.material.icons.extended)
+    implementation(libs.material3)
+    implementation(platform(libs.compose.bom))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-service:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    "gplayImplementation"(libs.gplay.review)
+    "gplayImplementation"(libs.gplay.review.ktx)
 
-    // Compose
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.ui:ui:${composeVersion}")
-    implementation("androidx.compose.ui:ui-tooling-preview:${composeVersion}")
-    implementation("androidx.compose.material3:material3:1.3.0")
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.32.0")
-    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.3")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("com.airbnb.android:lottie-compose:6.2.0")
-    implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("io.coil-kt:coil-gif:2.5.0")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
+    debugImplementation(libs.compose.ui.test.manifest)
+    debugImplementation(libs.compose.ui.tooling)
 
-    // Core dependencies
-    implementation("me.jahnen.libaums:core:0.10.0")
-    implementation("me.jahnen.libaums:libusbcommunication:0.3.0")
-    implementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.test.runner)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(platform(libs.compose.bom))
 
-    // Google Play
-    "gplayImplementation"("com.google.android.play:review:2.0.1")
-    "gplayImplementation"("com.google.android.play:review-ktx:2.0.1")
-
-    // Test dependencies
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testImplementation("androidx.test:core:1.6.1")
-    testImplementation("org.mockito:mockito-core:5.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.7.3")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
-    androidTestImplementation(platform("androidx.compose:compose-bom:${composeBomVersion}"))
-    debugImplementation("androidx.compose.ui:ui-tooling:${composeVersion}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${composeVersion}")
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.test.core)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
-buildscript {
-    val composeVersion by extra("1.5.15")
-    val composeBomVersion by extra("2024.09.03")
-    val kotlinVersion by extra("1.9.25")  // Also update the Gradle plugin version below!
-}
 plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
